82d1cc75206426f52a069b62372f78f113094d8f
rdelgador <ricardo.delgado@ucsp.edu.pe>
2016-04-27 16:43:34 -0500

    Heap Sort - Heapify

---
+++ Heap Sort/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+////////////HEAP SORT////////////////////
+
+/////MANTENIENDO LA PROPIEDAD DEL HEAP//////
+void max_heapify(int A[],int n,int i) //n -> tamaño de heap
+{
+    int largest=i;
+    int l = 2*i+1;
+    int r = 2*i+2;
+
+    if(l<n && A[r]>A[largest])
+        largest=l;
+    if(r<n && A[r]>A[largest])
+        largest=r;
+    if(largest!=i)
+    {
+        swap(A[i],A[largest]);
+        max_heapify(A,n,largest);
+    }
+
+}
+
+void build_max_heap(int A[], int n)
+{
+    for(int i=n/2 - 1; i>=0; i--)
+        max_heapify(A,n,i);
+
+    for(int i=n-1; i>=0;i--)
+    {
+        swap(A[0],A[i]);
+        max_heapify(A,i,0);
+    }
+    swap(A[0],A[1]);
+}
+
+void printA(int A[], int n)
+{
+    for(int i=0; i<n; ++i)
+        cout<<A[i]<<" ";
+    cout<<endl;
+}
+
+int main()
+{
+    int A[] = {4,1,3,2,16,9,10,14,8,7};
+    int n = sizeof(A)/sizeof(A[0]);
+
+    build_max_heap(A,n);
+
+    printA(A,n);
+}
